6a57ca02d59232913a3d0033857e07114c66b025
Florence MARTIN79 <florencemartin79@yahoo.fr>
2022-02-15 09:38:43 +0100

    creation of the js files in front to get the API elements ; the files are named following the script info in html files

--- front/js/script.js
+++ front/js/script.js
@@ -1,2 +1,22 @@
+/*appel des éléments de l'API sur port3000*/
 fetch("http://localhost:3000/api/products")
-    .then(response => console.log('test'))
+    .then(function (res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function (canapes) {
+        console.log(canapes);
+    })
+    .catch(function (err) {
+        console.log("erreur connexion API");
+    });
+
+
+
+
+/*Création d'un élément <a> enfant de l'élément dont l'ID est items via le DOM*/
+const newElt = document.createElement("a");
+let elt = document.getElementById("items");
+elt.appendChild(newElt);
+
