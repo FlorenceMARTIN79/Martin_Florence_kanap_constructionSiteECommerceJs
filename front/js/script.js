4d5451099d1a66c58b6dd70a63eac8fb41da553d
Florence MARTIN79 <florencemartin79@yahoo.fr>
2022-03-16 10:07:53 +0100

    put the functions on top of js file (outside the fetch) ; modif of the functions into one function ; modif of the nodes creating the DOM elements

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,35 @@
+//Functions creating the DOM elements needed for the index page
+function creationDomElementsIndex() {
+    //Creation of an a tag in the items section
+    const card = document.createElement("a");
+    card.setAttribute("href", "TO BE DETERMINED")
+    //Creation of an article in the card
+    const cardArticle = document.createElement("article");
+    //Creation of an img in the card
+    const cardImg = document.createElement("img");
+    //cardImg.setAttribute("src", kanapImg);
+    cardImg.setAttribute("alt", "photo of the sofa")
+    //Creation of a h3 in the card
+    const cardTitle = document.createElement("h3");
+    //cardTitle.setAttribute("class", kanapName);
+    cardTitle.setAttribute("id", "Nom");
+    //Creation of p in the card
+    const cardDescription = document.createElement("p");
+    cardDescription.setAttribute("class", "productDescription");
+    cardDescription.setAttribute("id", "description");
+    document.getElementById("items").appendChild(card);
+    card.appendChild(cardArticle);
+    cardArticle.appendChild(cardImg);
+    cardArticle.appendChild(cardTitle);
+    cardArticle.appendChild(cardDescription);
+}
+
+/*document.getElementById("items").appendChild(card);
+document.querySelector("section > a").appendChild(cardArticle);
+document.querySelector("a > article").appendChild(cardImg);
+document.querySelector("a > article").appendChild(cardTitle);
+document.querySelector("a > article").appendChild(cardDescription);*/
+
 /*appel des éléments de l'API sur port3000*/
 fetch("http://localhost:3000/api/products")
     .then(function (res) {
@@ -11,7 +43,7 @@ fetch("http://localhost:3000/api/products")
         console.log(sofas[1]);
         console.log(sofas.length);
 
-       
+
         /*Pour chaque objet du tableau on créé une carte DOM
         Une carte contient le nom, l'image et la description de l'objet*/
 
@@ -19,67 +51,15 @@ fetch("http://localhost:3000/api/products")
             var kanapName = sofas[i].name;
             var kanapDescription = sofas[i].description;
             var kanapImg = sofas[i].imageUrl;
+            creationDomElementsIndex (i);
+            
         }
 
-//Functions creating the DOM elements needed for the index page
-        function creationDomAIndex () {
-            //Creation of an a tag in the items section
-            let card = document.createElement("a");
-            document.getElementById("items").appendChild(card);
-            card.setAttribute("href", "TO BE DETERMINED")
-        }
-        
-        function creationDomArticleIndex () {
-            //Creation of an article in the card
-            const cardArticle = document.createElement("article");
-            document.querySelector("section > a").appendChild(cardArticle);
-        }
 
-        function creationDomImgIndex () {
-            //Creation of an img in the card
-            const cardImg = document.createElement("img");
-            document.querySelector("a > article").appendChild(cardImg);
-            cardImg.setAttribute("src", kanapImg);
-            cardImg.setAttribute("alt", "photo of the sofa")
-        }
 
-        function creationDomNameIndex () {
-            //Creation of a h3 in the card
-            const cardTitle = document.createElement("h3");
-            document.querySelector("a > article").appendChild(cardTitle);
-            cardTitle.setAttribute("class", kanapName);
-            cardTitle.setAttribute("id", "Nom");
-        }
-
-        function creationDomDescriptionIndex () {
-             //Creation of p in the card
-             const cardDescription = document.createElement("p");
-             document.querySelector("a > article").appendChild(cardDescription);
-             cardDescription.setAttribute("class", "productDescription");
-             cardDescription.setAttribute("id", "description");
-        }
-
-        //Loop creating as many DOM elements as sofas
-        for (let i = 0; i < sofas.length; i++) {
-            creationDomAIndex (i);
-        }
-
-        const numberOfA = sofas.length;
-
-        /*for (let i = 0; i < numberOfA; i++) {
-        creationDomArticleIndex (i);
-        }*/
-
-        /*for (let i = 0; i < sofas.length; i++) {
-            creationDomArticleIndex (i);
-        }*/
-        creationDomArticleIndex (1);
-        creationDomArticleIndex (1);
-        creationDomArticleIndex (1);
 
     })
     //Error message when the API has not been reached 
     .catch(function (err) {
         console.log("erreur connexion API");
     });
-
